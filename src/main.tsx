93dabfc040bf38c7e5bc55169e447b053fed7775
shashwa7-dev <shashwa7.dev@gmail.com>
2023-10-24 14:14:32 +0530

    auto updated pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { registerSW } from "virtual:pwa-register";
 
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
-      updateSW(true);
-    }
+    console.log("New content available. Reload?");
+    updateSW(true);
+    window.location.reload();
   },
   onOfflineReady() {
     console.log("offline ready");
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
